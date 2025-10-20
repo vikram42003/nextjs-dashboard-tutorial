ad2d23697b12bf17966d136f05fcc7da89d30276
vikram(wsl) <vikramjit360@gmail.com>
2025-10-20 14:59:59 +0530

    finishing changes

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -99,17 +99,11 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  try {
-    await sql`
+  await sql`
         UPDATE invoices
         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
         WHERE id = ${id}
       `;
-  } catch (error) {
-    // We'll also log the error to the console for now
-    console.error(error);
-    return { message: "Database Error: Failed to Update Invoice." };
-  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
